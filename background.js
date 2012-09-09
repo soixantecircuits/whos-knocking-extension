fe961e15927a158de97e19b8a6cf0b3f582c1c5f
Gabriel Delattre <gabriel@soixantecircuits.fr>
2012-09-09 22:59:07 +0200

    cleaning code

---
+++ background.js
@@ -0,0 +1,42 @@
+/*
+  Displays a notification with the message. Requires "notifications"
+  permission in the manifest file (or calling
+  "webkitNotifications.requestPermission" beforehand).
+*/
+
+function init(){
+  var socket = io.connect('http://whosknocking.herokuapp.com/');
+          socket.on('new_tweet', function (data) {
+            console.log(data);
+            var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
+            var hour = time[1] % 12 || 12;               // The prettyprinted hour.
+            var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
+            var notification = window.webkitNotifications.createNotification(
+              '48.png',                      // The image.
+              data.user.name + " is knocking !", // The title.
+              data.text      // The body.
+            );
+            notification.show();
+   });
+   var notification = window.webkitNotifications.createNotification(
+              '48.png',                      // The image.
+              "Who's Knocking initialization ...", // The title.
+              "Welcome ! Hope you will enjoy your daily notifications."      // The body.
+            );
+            notification.show();
+}
+// Conditionally initialize the options.
+if (!localStorage.isInitialized) {
+  localStorage.isActivated = true;   // The display activation.
+  localStorage.isInitialized = true; // The option initialization.
+}
+
+// Test for notification support.
+if (window.webkitNotifications) {
+    if (JSON.parse(localStorage.isActivated)) {
+      init();
+
+    }
+}else{
+  alert("Sorry you need to upgrade your system. It seems notification support is not enable.")
+}
